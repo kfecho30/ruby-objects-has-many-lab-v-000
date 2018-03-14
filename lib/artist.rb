2642a6962cc416ed573fc805250fdc9479a92958
Kelsey Fecho <kfecho30@gmail.com>
2018-03-14 17:56:17 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,14 +11,14 @@ class Artist
   def add_song(song)
     song.artist = self
     @songs << song
-    @@songs += 1
+    @@song_count += 1
   end
   def add_song_by_name(name)
     song = Song.new(name)
     song.artist = self
     song.artist_name = self.name
     @songs << song
-    @@songs +=1
+    @@song_count +=1
   end
   def self.song_count
     @@song_count
