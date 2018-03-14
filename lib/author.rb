d3745e2b1080720e49831d7cf12d105ec397c982
Kelsey Fecho <kfecho30@gmail.com>
2018-03-14 18:01:30 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,10 +1,15 @@
 class Author
   attr_accessor :name
+  @@post_count = 0
   def initialize(name)
     @name = name
     @posts = []
   end
   def add_post(post)
     @posts << post
+    @@post_count += 1
+  end
+  def self.post_count
+    @@post_count
   end
 end
