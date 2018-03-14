7bfc78d752d894f6ec8017ea1b61cb5f2f51d3ce
Kelsey Fecho <kfecho30@gmail.com>
2018-03-14 17:53:47 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,6 @@
 class Artist
   attr_accessor :name
+  @@songs = 0
   def initialize(name)
     @name = name
     @songs = []
@@ -10,11 +11,16 @@ class Artist
   def add_song(song)
     song.artist = self
     @songs << song
+    @@songs += 1
   end
   def add_song_by_name(name)
     song = Song.new(name)
     song.artist = self
     song.artist_name = self.name
     @songs << song
+    @@songs +=1
+  end
+  def song_count
+    @@songs.length
   end
 end
